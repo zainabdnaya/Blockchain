470e778ad42bb5319184814af4c76487b6e11060
zdnaya <diyanazizoz13@gmail.com>
2022-08-29 10:12:14 +0100

    signer

--- tests/singer.js
+++ tests/singer.js
@@ -1,8 +1,16 @@
-var myLog = new File("/data/logs/today.log");
 
 // See if the file exists
-if(myLog.exists()){
-  write('The file exists');
-}else{
-  write('The file does not exist');
+//var myLog = new File(file);
+const fs = require("fs");
+
+function hash(file)
+{
+    const path = file;
+    if (fs.existsSync(path)) {
+      console.log("exists:", path);
+    } else {
+      console.log("DOES NOT exist:", path);
+    }
 }
+
+hash("res.txt")
