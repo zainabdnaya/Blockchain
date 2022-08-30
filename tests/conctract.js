7899a6819b0f8f4f9a0c00ef5d9645327214839a
Zainab Dnaya <diyanazizo13@gmail.com>
2022-08-30 10:23:23 +0100

    smartcontractsolidity

---
+++ tests/conctract.js
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.4;
+
+contract TimeAndPlace {
+    string place;
+    uint time;
+
+    constructor(uint time1, string memory festival) public{
+        time = time1;    
+        place = festival;
+    }
+
+    function getStartTime() public view returns (uint time) {
+        return (time);
+    }
+
+    function getPlace() public view returns (string memory place) {
+        return (place);
+    }
+
+}
